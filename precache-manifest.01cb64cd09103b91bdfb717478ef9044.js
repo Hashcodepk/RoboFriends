339c06446e9bfa66e2449416b56ef3a1735017c6
hashcodepk <hashirlives@gmail.com>
2019-02-06 03:42:09 +0500

    Updates

---
+++ precache-manifest.01cb64cd09103b91bdfb717478ef9044.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "877cac2a9c44d7342a3d",
+    "url": "/RoboFriends/static/js/runtime~main.877cac2a.js"
+  },
+  {
+    "revision": "3c74fdb0cd935c389c62",
+    "url": "/RoboFriends/static/js/main.3c74fdb0.chunk.js"
+  },
+  {
+    "revision": "2094677591e8d1e3cc28",
+    "url": "/RoboFriends/static/js/1.20946775.chunk.js"
+  },
+  {
+    "revision": "3c74fdb0cd935c389c62",
+    "url": "/RoboFriends/static/css/main.44ec1132.chunk.css"
+  },
+  {
+    "revision": "2094677591e8d1e3cc28",
+    "url": "/RoboFriends/static/css/1.c4533bdd.chunk.css"
+  },
+  {
+    "revision": "29646fce0f4d417a49c07e2ec9cf322f",
+    "url": "/RoboFriends/index.html"
+  }
+];
